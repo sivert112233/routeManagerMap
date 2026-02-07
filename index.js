3a77f624974f20e787c5dbd736ea9ee8e19370c8
Sivert <sivert112233@gmail.com>
2026-02-07 15:14:12 +0100

    new loc search

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 import { routeData } from "./data.js";
 
+
+// Declaration and init map location.
 const map = L.map('map').setView([60.908479414005015, 10.810253805299897], 7);
 const inputBoxButton = document.querySelector('.inputBoxButton');
 const inputBoxInput = document.querySelector('.inputBoxInput');
 const displayBox = document.querySelector('.displayBox');
 
+//Adding map to the pages.
 new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+//Search functiion.
 async function searchLocationApi(x) {
-    displayBox.innerHTML = '';
+
+    //Message to user...............?
+    displayBox.innerHTML = `
+        <div class="displayBoxRender">
+                <div class="displayBoxLocNumber">Flere lokasjoner har samme navn. finn rett lokasjon fra listen under. vis den ikke et der, spesifiser søket ditt!</div>
+        </div>
+        
+    `;
+
+    //Getting search location(s) from the api.
     const url = `https://nominatim.openstreetmap.org/search?q=${x}&format=json&limit=25`;
     const response = await fetch(url).then(respons => respons.json());
-    let locNum = 0;
+
+    
     if (response.length > 1) {
         response.forEach(x => {
-            locNum++
-            console.log(x.display_name);
-            console.log(response[x]);
+            //Displaying search result on the page.
             displayBox.innerHTML += `
                 <div class="displayBoxRender">
-                    <div class="displayBoxLocNumber">Location: ${locNum}</div>
-                    <div class="displayBoxLocResult">${x.display_name}</div>
+                    <button class="displayBoxLocResultButton" value="${x.lat} ${x.lon}">
+                        <div class="displayBoxLocResult">${x.display_name}</div>
+                    </button>
                 </div>
             `;
-        });
+        
+        }); 
     } else {
+        //Adding marker to the map.
         L.marker([response[0].lat, response[0].lon]).addTo(map);
     }
+    
+    document.querySelectorAll('.displayBoxLocResultButton').forEach((x) => {
+        x.addEventListener('click',() => {
+            const lat = x.value.slice(0, x.value.search(/ /));
+            const lon = x.value.slice(x.value.search(/ /) + 1, x.value.length);
+            L.marker([lat, lon]).addTo(map);
+        });
+    });
+    
+    
 }
 
+//Search button event.
 inputBoxButton.addEventListener('click', () => {
     searchLocationApi(inputBoxInput.value);
 });
